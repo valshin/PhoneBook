3d31a9d66337e2290a38b1c3617278ab16d6ea11
valshin <valshin2@gmail.com>
2015-11-22 21:20:15 +0200

    [+] begin FileDB test creating

--- src/test/java/me/noip/valshin/db/services/FileDBTest.java
+++ src/test/java/me/noip/valshin/db/services/FileDBTest.java
@@ -1,5 +1,23 @@
 package me.noip.valshin.db.services;
 
-public class FileDBTest {
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mockito;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import me.noip.valshin.MvcConfigTest;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MvcConfigTest.class)
+public class FileDBTest {
+	
+	@InjectMocks
+    private String path = "/testFileDB.db";
+	@InjectMocks
+    private ObjectMapper mapper = Mockito.mock(ObjectMapper.class);
+	
+	
 }
