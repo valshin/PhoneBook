a5f245c53f46adcb43a806bcc95984d626c23f7f
valshin <valshin2@gmail.com>
2015-12-03 12:06:37 +0200

    [+] single page alpha 2

--- src/main/java/me/noip/valshin/config/SecurityConfig.java
+++ src/main/java/me/noip/valshin/config/SecurityConfig.java
@@ -51,39 +51,8 @@ public class SecurityConfig {
         @Override
 		protected void configure(HttpSecurity http) throws Exception {
 			http.httpBasic().and().authorizeRequests()
-					.antMatchers("/user", "/adduser", "/register.html", "/index.html", "/home.html", "/log_in.html", "/").permitAll().anyRequest()
-					.authenticated().and().logout().and().csrf()
-					.csrfTokenRepository(csrfTokenRepository()).and()
-					.addFilterBefore(csrfHeaderFilter(), CsrfFilter.class);
-		}
-
-		private Filter csrfHeaderFilter() {
-			return new OncePerRequestFilter() {
-				@Override
-				protected void doFilterInternal(HttpServletRequest request,
-						HttpServletResponse response, FilterChain filterChain)
-						throws ServletException, IOException {
-					CsrfToken csrf = (CsrfToken) request.getAttribute(CsrfToken.class
-							.getName());
-					if (csrf != null) {
-						Cookie cookie = WebUtils.getCookie(request, "XSRF-TOKEN");
-						String token = csrf.getToken();
-						if (cookie == null || token != null
-								&& !token.equals(cookie.getValue())) {
-							cookie = new Cookie("XSRF-TOKEN", token);
-							cookie.setPath("/");
-							response.addCookie(cookie);
-						}
-					}
-					filterChain.doFilter(request, response);
-				}
-			};
-		}
-
-		private CsrfTokenRepository csrfTokenRepository() {
-			HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
-			repository.setHeaderName("X-XSRF-TOKEN");
-			return repository;
+					.antMatchers("/adduser", "/register.html", "/index.html", "/home.html", "/log_in.html", "/").permitAll().anyRequest()
+					.authenticated().and().logout().logoutSuccessUrl("/").and().csrf().disable();
 		}
     }
 }
--- src/main/resources/static/app/loginCtrl.js
+++ src/main/resources/static/app/loginCtrl.js
@@ -1,54 +1,16 @@
 app.controller(
     'loginCtrl',
-    function ($rootScope, $scope, $http, $location, $route) {
-
-        $scope.tab = function (route) {
-            return $route.current && route === $route.current.controller;
-        };
-
-        var authenticate = function (credentials, callback) {
-            debugger;
-            var headers = credentials ? {
-                authorization: "Basic "
-                + btoa(credentials.username + ":"
-                    + credentials.password)
-            } : {};
-
-            $http.get('user', {
-                headers: headers
-            }).success(function (data) {
-                debugger;
-                if (data.name) {
-                    $rootScope.authenticated = true;
-                } else {
-                    $rootScope.authenticated = false;
-                }
-                callback && callback($rootScope.authenticated);
-            }).error(function () {
-                debugger;
-                $rootScope.authenticated = false;
-                callback && callback(false);
-            });
-
-        };
-
-        authenticate();
-
-        $scope.credentials = {};
+    function ($rootScope, $scope, $state) {
         $scope.login = function () {
-            authenticate($scope.credentials, function (authenticated) {
-                if (authenticated) {
-                    console.log("Login succeeded")
-                    $location.path("/");
-                    $scope.error = false;
-                    $rootScope.authenticated = true;
+            $rootScope.authenticate($scope.credentials, function(){
+                if (!$rootScope.authenticated){
+                    //$rootScope.setTabs('login');
+                    $state.go('login');
                 } else {
-                    console.log("Login failed")
-                    $location.path("/login");
-                    $scope.error = true;
-                    $rootScope.authenticated = false;
+                    //$rootScope.setTabs('phonebook');
+                    $state.go('phonebook');
                 }
-            })
+            });
         };
     }
 );
